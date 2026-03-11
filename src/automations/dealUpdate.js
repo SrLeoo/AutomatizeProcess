27639fdef14d89f9769003f2f74d9aec034d0fff
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-11 13:20:18 -0300

    refactor: function alterarPrioridade

--- src/automations/dealUpdate.js
+++ src/automations/dealUpdate.js
@@ -1,51 +1,41 @@
-const axios = require("axios");
-
 module.exports = async function dealUpdate(body) {
     const dealId = body?.data?.FIELDS?.ID;
 
-    axios.get(`${process.env.BITRIX_WEBHOOK}crm.deal.get?ID=${dealId}`)
-        .then(response => {
-            const dealData = response.data.result;
+    getDeal(dealId).then(map => {
+        alterarPrioridade(map, dealId);
+    });
+};
 
-            const title = dealData.TITLE;
-            const UF_CRM_1761801450 = dealData.UF_CRM_1761801450;
+function alterarPrioridade(map, dealId) {
+    let updatedTitle = null;
 
-            if (UF_CRM_1761801450 === "185") {
+    if (map.prioridade === "185") {
+        if (!map.title.startsWith("♨️")) {
+            updatedTitle = `♨️ ${map.title}`;
+        }
+    } else {
+        if (map.title.startsWith("♨️")) {
+            updatedTitle = map.title.replace("♨️ ", "");
+        }
+    }
 
-                if (!title.startsWith("♨️")) {
-                    console.log("Adicionando prioridade negócio:", dealId);
-                    const updatedTitle = `♨️ ${title}`;
-                    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
-                        id: dealId,
-                        fields: {
-                            TITLE: updatedTitle
-                        }
-                    });
+    if (!updatedTitle) {
+        return;
+    }
 
-                }
-
-            } else {
-                if (title.startsWith("♨️")) {
-                    console.log("Removendo prioridade negócio:", dealId);
-                    const updatedTitle = title.replace("♨️ ", "");
-                    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
-                        id: dealId,
-                        fields: {
-                            TITLE: updatedTitle
-                        }
-                    });
-
-                }
-
-            }
-
-        });
+    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
+        id: dealId,
+        fields: {
+            TITLE: updatedTitle
+        }
+    });
 }
-function mapDeal(dealData) {
-    return {
-        id: dealData.ID,
-        title: dealData.TITLE,
-        temperatura: dealData.UF_CRM_1761801450,
-        raw: dealData
-    };
+
+function teste(map, dealId) {
+    console.log({
+        step: "DEBUG_AUTOMACAO",
+        dealId: dealId,
+        prioridade: map?.prioridade,
+        title: map?.title
+    });
 }
