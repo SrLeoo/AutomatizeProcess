e6aa7e54011c978d967812d6fb03bf1bba719b5a
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-11 12:35:13 -0300

    feat: ajuste função de prioridade

--- src/automations/dealUpdate.js
+++ src/automations/dealUpdate.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async function dealUpdate(body) {
-const dealId = body?.data?.FIELDS?.ID;
+    const dealId = body?.data?.FIELDS?.ID;
 
     axios.get(`${process.env.BITRIX_WEBHOOK}crm.deal.get?ID=${dealId}`)
         .then(response => {
@@ -11,29 +11,35 @@ const dealId = body?.data?.FIELDS?.ID;
             const UF_CRM_1761801450 = dealData.UF_CRM_1761801450;
 
             if (UF_CRM_1761801450 === "185") {
-                if (title.startsWith("♨️")) {
-                    if (UF_CRM_1761801450 != "185") {
-                        const updatedTitle = title.replace("♨️ ", "");
-                        axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
-                            ID: dealId,
-                            fields: {
-                                TITLE: updatedTitle
-                            }
-                        });
-                    }
+
+                if (!title.startsWith("♨️")) {
+                    console.log("Adicionando prioridade negócio:", dealId);
+                    const updatedTitle = `♨️ ${title}`;
+                    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
+                        id: dealId,
+                        fields: {
+                            TITLE: updatedTitle
+                        }
+                    });
+
                 }
-                const updatedTitle = `♨️ ${title}`;
-                axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
-                    ID: dealId,
-                    fields: {
-                        TITLE: updatedTitle
+
+            } else {
+                if (title.startsWith("♨️")) {
+                    console.log("Removendo prioridade negócio:", dealId);
+                    const updatedTitle = title.replace("♨️ ", "");
+                    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
+                        id: dealId,
+                        fields: {
+                            TITLE: updatedTitle
+                        }
+                    });
+
+                }
+
             }
-          }
-        );
 
-      }
-
-    });
+        });
 }
 function mapDeal(dealData) {
     return {
