e8d9f785d4bb3fadef8d6cc94b8b5d82d53a4600
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-11 05:09:59 -0300

    testv6

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require("express");
-const axios = require("axios");
 require("dotenv").config();
 
+const webhookRouter = require("./src/services/router");
+
 const app = express();
-
-const PORT = process.env.PORT;
-
-const webhookRouter = require("../AutomatizeProcess/src/services/router.js");
-
-const BITRIX_WEBHOOK = process.env.BITRIX_WEBHOOK;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
-
   webhookRouter(req.body);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor iniciado");
 });
