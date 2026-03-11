ca85f69cbf86f8b40be8d031633a716bc640b194
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-11 04:28:52 -0300

    testv3

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+const BITRIX_WEBHOOK = process.env.BITRIX_WEBHOOK;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/", (req, res) => {
-  console.log("Novo webhook recebido do Bitrix");
-  console.log("Body recebido:");
-  console.log(JSON.stringify(req.body, null, 2));
+app.post("/", async (req, res) => {
+  try {
+    console.log("Body recebido:");
+    console.log(JSON.stringify(req.body, null, 2));
 
+    const dealId = req.body.data.FIELDS.ID;
+
+    // consulta deal no Bitrix
+    const response = await axios.get(
+      `${BITRIX_WEBHOOK}crm.deal.get?id=${dealId}`
+    );
+
+    console.log("Resposta do Bitrix:");
+    console.log(JSON.stringify(response.data, null, 2));
+
+  } catch (error) {
+    console.log("Erro ao consultar deal:", error.message);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor iniciado na porta ${PORT}`);
 });
