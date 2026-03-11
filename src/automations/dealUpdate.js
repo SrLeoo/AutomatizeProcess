7594c5f98daf95f4d19d2fd0145424c9c5cbd709
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-11 13:21:26 -0300

    refactor: function alterarPrioridade

--- src/automations/dealUpdate.js
+++ src/automations/dealUpdate.js
@@ -1,41 +1,46 @@
+const axios = require("axios");
+
+function getDeal(dealId) {
+    return axios
+        .get(`${process.env.BITRIX_WEBHOOK}crm.deal.get?id=${dealId}`)
+        .then(response => {
+            const dealData = response.data.result;
+
+            return {
+                id: dealData.ID,
+                title: dealData.TITLE,
+                prioridade: dealData.UF_CRM_1761801450
+            };
+        });
+}
+
 module.exports = async function dealUpdate(body) {
     const dealId = body?.data?.FIELDS?.ID;
 
     getDeal(dealId).then(map => {
-        alterarPrioridade(map, dealId);
+        if (map.prioridade === "185") {
+            if (!map.title.startsWith("♨️")) {
+                // console.log("Adicionando prioridade negócio:", dealId);
+                const updatedTitle = `♨️ ${map.title}`;
+                axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
+                    id: dealId,
+                    fields: {
+                        TITLE: updatedTitle
+                    }
+                });
+            }
+
+        } else {
+            if (map.title.startsWith("♨️")) {
+                // console.log("Removendo prioridade negócio:", dealId);
+                const updatedTitle = map.title.replace("♨️ ", "");
+                axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
+                    id: dealId,
+                    fields: {
+                        TITLE: updatedTitle
+                    }
+                });
+            }
+        }
     });
 };
-
-function alterarPrioridade(map, dealId) {
-    let updatedTitle = null;
-
-    if (map.prioridade === "185") {
-        if (!map.title.startsWith("♨️")) {
-            updatedTitle = `♨️ ${map.title}`;
-        }
-    } else {
-        if (map.title.startsWith("♨️")) {
-            updatedTitle = map.title.replace("♨️ ", "");
-        }
-    }
-
-    if (!updatedTitle) {
-        return;
-    }
-
-    axios.post(`${process.env.BITRIX_WEBHOOK}crm.deal.update`, {
-        id: dealId,
-        fields: {
-            TITLE: updatedTitle
-        }
-    });
-}
-
-function teste(map, dealId) {
-    console.log({
-        step: "DEBUG_AUTOMACAO",
-        dealId: dealId,
-        prioridade: map?.prioridade,
-        title: map?.title
-    });
-}
