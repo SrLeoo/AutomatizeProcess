0bc500de7c7c6cbc688f6a7c3fa2ff9bcee5b215
Leonardo Magalhaes Marchi <leonardo.mmm2013@gmail.com>
2026-03-13 06:19:50 -0300

    test: Validação de fatura2 (Deal -> Company -> Invoice)

--- src/automations/deal/dealUpdate.js
+++ src/automations/deal/dealUpdate.js
@@ -2,12 +2,10 @@ const axios = require("axios");
 const getDeal = require("../../services/bitrix/deal/getDeal");
 const getCompany = require("../../services/bitrix/company/getCompany");
 const getInvoice = require("../../services/bitrix/invoice/getInvoice");
-
-const listInvoice = require("../../services/bitrix/invoice/listInvoice");
+const listInvoices = require("../../services/bitrix/invoice/listInvoices");
 
 module.exports = async function dealUpdate(body) {
     const dealId = body?.data?.FIELDS?.ID;
-    console.log("dealUpdate acionada para o negócio ID:", dealId);
 
     if (!dealId) {
         return;
@@ -16,8 +14,6 @@ module.exports = async function dealUpdate(body) {
     // Busca dados
     const mapDeal = await getDeal(dealId);
     const mapCompany = await getCompany(mapDeal.raw.COMPANY_ID);
-    const mapInvoice = await getInvoice(mapDeal.raw.INVOICE_ID);
-    const listInvoice = await listInvoice(mapDeal.raw.INVOICE_ID);
 
     // Automação: Atualizar título do negócio com base na prioridade
     if (mapDeal.prioridade === "185") {
@@ -44,7 +40,6 @@ module.exports = async function dealUpdate(body) {
 
     // Automação: Somar em fatura o tempo do negócio
     if (mapDeal.stageId === "WON") {
-
         const invoiceId = mapCompany?.UF_CRM_1773391522;
 
         if (!invoiceId) {
