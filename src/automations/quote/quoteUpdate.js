15be6f2413e9b15814307090e69672538e9d72e0
Magalhaes <leonardo.mmm2013@gmail.com>
2026-03-21 07:45:37 -0300

    fix: calc temp * hora

--- src/automations/quote/quoteUpdate.js
+++ src/automations/quote/quoteUpdate.js
@@ -28,7 +28,7 @@ module.exports = async function quoteUpdate(body) {
     const tempo = Number(mapQuote.raw.UF_CRM_1773953900);
     const valorHora = Number(mapCompany.valorHora);
     if (tempo > 0 && valorHora > 0) {
-        const montante = (tempo / 60) * valorHora;
+        const montante = (tempo * valorHora);
         console.log(`Quote ${quoteId}: ${tempo}min = R$ ${montante.toFixed(2)}`);
         await axios.post(`${process.env.BITRIX_WEBHOOK}crm.quote.update`, {
             id: quoteId,
